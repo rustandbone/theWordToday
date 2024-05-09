fa56ae2680ace64ca506fd3b8b75d80a215fe132
rustandbone <dlehdgh14@naver.com>
2024-05-10 01:40:59 +0900

    feat: 메타데이터 수정

--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -1,33 +1,35 @@
 'use client';
+import Head from 'next/head';
 import ImageComponent from '@/app/component/ImageComponent.client';
 import { wordImages } from '@/app/data';
-import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-export default function Word({ imageUrl }: { imageUrl: string }) {
-  <Head>
-    <title>당일말씀</title>
-    <meta name="description" content="오늘의 말씀과 함께 하루를" />
-    <meta property="og:title" content="당일말씀" />
-    <meta property="og:description" content="오늘의 말씀과 함께 하루를" />
-    <meta property="og:image" content={imageUrl} />
-  </Head>;
+export default function Word() {
+  const [imageUrl, setImageUrl] = useState('');
 
-  return <ImageComponent src={`/word${imageUrl}`} />;
-}
+  useEffect(() => {
+    const currentDate = new Date();
+    const startOfYear = new Date(currentDate.getFullYear(), 0, 0);
+    const diff = Number(currentDate) - Number(startOfYear);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
 
-export async function getServerSideProps() {
-  const currentDate = new Date();
-  const startOfYear = new Date(currentDate.getFullYear(), 0, 0);
-  const diff = Number(currentDate) - Number(startOfYear);
-  const oneDay = 1000 * 60 * 60 * 24;
-  const dayOfYear = Math.floor(diff / oneDay);
+    const index = dayOfYear % wordImages.length;
 
-  const index = dayOfYear % wordImages.length;
-  const imageUrl = wordImages[index];
+    setImageUrl(wordImages[index]);
+  }, []);
 
-  return {
-    props: {
-      imageUrl: `/word${imageUrl}`, // 이미지 URL을 props로 전달
-    },
-  };
+  return (
+    <>
+      <Head>
+        <title>당일말씀</title>
+        <meta name="description" content="오늘의 말씀과 함께 하루를" />
+        <meta property="og:title" content="당일말씀" />
+        <meta property="og:description" content="오늘의 말씀과 함께 하루를" />
+        <meta property="og:image" content={imageUrl} />
+      </Head>
+      
+      <ImageComponent src={`/word${imageUrl}`} />
+    </>
+  );
 }
