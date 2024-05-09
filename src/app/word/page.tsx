c1945d9e66cbb3685276d1607bc9f38b8cc4c573
rustandbone <dlehdgh14@naver.com>
2024-05-10 00:35:10 +0900

    feat: 날짜별 난수 생성, 매일 다른 이미지 불러오기

--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -1,5 +1,22 @@
+'use client';
 import ImageComponent from '@/app/component/ImageComponent.client';
+import { wordImages } from '@/app/data';
+import { useEffect, useState } from 'react';
 
-export default function word() {
-  return <ImageComponent src="/이 생명의 말씀을 다 백성에게.png" />;
+export default function Word() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const startOfYear = new Date(currentDate.getFullYear(), 0, 0);
+    const diff = Number(currentDate) - Number(startOfYear);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
+
+    const index = dayOfYear % wordImages.length;
+
+    setImageUrl(wordImages[index]);
+  }, []);
+
+  return <ImageComponent src={imageUrl} />;
 }
