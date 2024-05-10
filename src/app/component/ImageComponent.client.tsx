7a49314c0513b4a01cd3e562ae8b0480599e0511
rustandbone <dlehdgh14@naver.com>
2024-05-10 22:33:21 +0900

    feat: 말씀 버튼 기능 추가(뒤로 가기, 다운로드, 공유), Toaster, 상수 적용

--- src/app/component/ImageComponent.client.tsx
+++ src/app/component/ImageComponent.client.tsx
@@ -1,19 +1,67 @@
 'use client';
 import Image from 'next/image';
 import styles from '@/app/page.module.css';
+import toast from 'react-hot-toast';
 import { usePalette } from 'color-thief-react';
 import { FaArrowLeft, FaDownload, FaShare } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+import { WORD_URL } from '@/app/constants';
 
 export default function ImageComponent({ src }: { src: string }) {
-  const { data } = usePalette(src, 2, 'hex');
-  const imgAlt = src.split('.')[0].replace('/', '');
+  const { data: colors } = usePalette(src, 2, 'hex');
+  const router = useRouter();
+  const articleRef = useRef<HTMLElement | null>(null);
 
-  if (data) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (articleRef.current) {
+        articleRef.current.classList.add(styles.fadeOut);
+      }
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const imgAlt = src.split('.')[0].replace('/word/', '');
+
+  const goBack = () => {
+    router.push('/');
+  };
+
+  const downloadImage = () => {
+    const imageLink = document.createElement('a');
+    const imageUrl = src;
+    imageLink.href = imageUrl;
+    imageLink.download = imgAlt;
+    document.body.appendChild(imageLink);
+    imageLink.click();
+    document.body.removeChild(imageLink);
+
+    toast.success('당일말씀 이미지가 저장되었습니다.', {
+      id: 'download',
+    });
+  };
+
+  const sharePage = () => {
+    navigator.clipboard
+      .writeText(WORD_URL)
+      .then(() => {
+        toast.success('링크가 클립보드에 복사되었습니다.', {
+          id: 'copy',
+        });
+      })
+      .catch((error) => {
+        console.error('클립보드 복사 실패:', error);
+      });
+  };
+
+  if (colors) {
     return (
       <main
         className={styles.main}
         style={{
-          background: `radial-gradient(circle, ${data[0]}, ${data[1]})`,
+          background: `radial-gradient(circle, ${colors[0]}, ${colors[1]})`,
         }}
       >
         <Image
@@ -23,17 +71,18 @@ export default function ImageComponent({ src }: { src: string }) {
           style={{ objectFit: 'contain' }}
           priority={true}
         />
-        <section className={styles.icons}>
-          <button type="button">
+
+        <article className={styles.icons} ref={articleRef}>
+          <button type="button" onClick={goBack}>
             <FaArrowLeft className={styles.icon} />
           </button>
-          <button type="button">
+          <button type="button" onClick={downloadImage}>
             <FaDownload className={styles.icon} />
           </button>
-          <button type="button">
+          <button type="button" onClick={sharePage}>
             <FaShare className={styles.icon} />
           </button>
-        </section>
+        </article>
       </main>
     );
   }
