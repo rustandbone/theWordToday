a4abfc895c3ed503a27ce061f8a9d8f89e67e08b
rustandbone <dlehdgh14@naver.com>
2024-05-13 00:27:20 +0900

    style: 안내 문구 수정 및 아이콘 색상 변경

--- src/app/component/Info.tsx
+++ src/app/component/Info.tsx
@@ -5,18 +5,20 @@ import { IoClose } from 'react-icons/io5';
 export default function Info({ hideArticle }: { hideArticle: () => void }) {
   return (
     <p>
-      당일말씀에는
+      [당일말씀]에는
       <br />
       매일 새로운 말씀이 등장합니다.
       <br />
       <br />
       하루를 온전히 살아가기 위한
       <br />
-      당일말씀과 함께 하세요!
+      [당일말씀]과 함께 하세요!
       <br />
       <br />
       <span className={styles.infoWord}>
-        오늘 내가 네게 명하는 이 말씀을 너는 마음에 새기고
+        오늘 내가 네게 명하는 이 말씀을
+        <br />
+        너는 마음에 새기고
         <br />
         &lt;신명기 6장 6절&gt;
       </span>
